617ed4bf27e3d0a87fa429a19d4b018b447f98e9
Adel NOUAR <adel.nouar@protonmail.com>
2021-05-19 00:59:36 +0200

    Adding function get last amount

--- blockchain.py
+++ blockchain.py
@@ -1,11 +1,16 @@
-blockchain = [1]
+blockchain = []
 
 
-def add_value():
-    blockchain.append([blockchain[0], 5.3])
-    print(blockchain)
+def get_last_blockchain_value():
+    return blockchain[-1]
 
 
-add_value()
-add_value()
-add_value()
+def add_value(transaction_amount, last_transaction=[1]):
+    blockchain.append([last_transaction, transaction_amount])
+
+
+add_value(2)
+add_value(0.9, get_last_blockchain_value())
+add_value(10.89, get_last_blockchain_value())
+
+print(blockchain)
