f0f53c1bdbe4ed7960640beb54b080bdf5ca3c69
Adel NOUAR <adel.nouar@protonmail.com>
2021-05-20 09:37:31 +0200

    Adding comments

--- blockchain.py
+++ blockchain.py
@@ -1,15 +1,25 @@
+# Initializing our blockchain list
 blockchain = []
 
 
 def get_last_blockchain_value():
+    """ Returns the last value of the current blockchain. """
     return blockchain[-1]
 
 
 def add_value(transaction_amount, last_transaction=[1]):
+    """ Append a new value as well as the last blockchain value to the blockchain. 
+    
+    Arguments: 
+        :transaction_amount: The amount that should be added.
+        :last_transaction: The last blockchain transaction (default [1]).
+    """
+
     blockchain.append([last_transaction, transaction_amount])
 
 
 def get_user_input():
+    """ Returns the input of the user (a new transaction amount) as a float. """
     return float(input('Your transaction amount please: '))
 
 
