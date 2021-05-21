bca9c24171da84dcb1108df285472c59478e3a45
Adel NOUAR <adel.nouar@protonmail.com>
2021-05-22 00:31:31 +0200

    + adding infinite block, + adding printing each blocks

--- blockchain.py
+++ blockchain.py
@@ -26,11 +26,18 @@ def get_user_input():
 tx_amount = get_user_input()
 add_value(tx_amount)
 
-tx_amount = get_user_input()
-add_value(last_transaction=get_last_blockchain_value(),
-          transaction_amount=tx_amount)
 
+
+# Get the third transaction input and add the value to the blockchain
 tx_amount = get_user_input()
 add_value(tx_amount, get_last_blockchain_value())
 
-print(blockchain)
+while True:
+    tx_amount = get_user_input()
+    add_value(tx_amount, get_last_blockchain_value())
+    
+    # Output the blockchain list to the console
+    for block in blockchain:
+        print('Outputing Block')
+        print(block)
+print('Done')
