b6ca1661fd8978fa75b201cc8f450d5259e88069
Adel NOUAR <adel.nouar@protonmail.com>
2021-05-23 16:38:40 +0200

    Adding mine block

--- blockchain.py
+++ blockchain.py
@@ -1,5 +1,12 @@
 # Initializing our blockchain list
-blockchain = []
+genesis_block = {
+    'previous_hash': '',
+    'index': 0,
+    'transactions': []
+}
+blockchain = [genesis_block]
+open_transaction = []
+owner = 'Someone'
 
 
 def get_last_blockchain_value():
@@ -9,21 +16,40 @@ def get_last_blockchain_value():
     return blockchain[-1]
 
 
-def add_transaction(transaction_amount, last_transaction=[1]):
+def add_transaction(recipient, sender=owner, amount=1.0):
     """ Append a new value as well as the last blockchain value to the blockchain.
 
     Arguments:
-        :transaction_amount: The amount that should be added.
-        :last_transaction: The last blockchain transaction (default [1]).
+        :sender: The sender of the coins.
+        :recipient: The recipient of the coins.
+        :amount: The amount of coins sent with the transaction(default = 1.0)
     """
-    if last_transaction == None:
-        last_transaction = [1]
-    blockchain.append([last_transaction, transaction_amount])
+    transaction = {
+        'sender': sender,
+        'recipient': recipient,
+        'amount': amount}
+    open_transaction.append(transaction)
+
+
+def mine_block():
+    last_block = blockchain[-1]
+    hashed_block = ''
+    for key in last_block:
+        value = last_block[key]
+        hashed_block = hashed_block + str(value)
+    block = {
+        'previous_hash': hashed_block,
+        'index': len(blockchain),
+        'transactions': open_transaction
+    }
+    blockchain.append(block)
 
 
 def get_transaction_value():
     """ Returns the input of the user (a new transaction amount) as a float. """
-    return float(input('Your transaction amount please: '))
+    recipient = input('Enter the recipient of the transaction: ')
+    amount = float(input('Your transaction amount please: '))
+    return recipient, amount
 
 
 def get_user_choice():
@@ -67,13 +93,17 @@ user_choice = None
 while user_choice != 'q':
     print('Please choose:')
     print('1: Add a new transaction value')
-    print('2: Output the blockchain blocks')
+    print('2: Mine a new block')
+    print('3: Output the blockchain blocks')
     print('h: Manipulate the chain')
     print('q: Quit')
     user_choice = get_user_choice()
     if user_choice == '1':
-        add_transaction(get_transaction_value(), get_last_blockchain_value())
+        recipient, amount = get_transaction_value()
+        add_transaction(recipient, amount=amount)
     elif user_choice == '2':
+        mine_block()
+    elif user_choice == '3':
         print_blockchain_elements()
     elif user_choice == 'h':
         if len(blockchain) >= 1:
@@ -82,9 +112,9 @@ while user_choice != 'q':
         break
     else:
         print('Input was invalid, please pick a value from the list!')
-    if not verify_chain():
-        print('Invalid blockchain!')
-        break
+    # if not verify_chain():
+    #     print('Invalid blockchain!')
+    #     break
 else:
     print('User left!')
 print('Done')
