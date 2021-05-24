2ec43e99474f76ed8e16c0655026f57ca9637f90
Adel NOUAR <adel.nouar@protonmail.com>
2021-05-24 14:07:43 +0200

    Adding simplified mining

--- blockchain.py
+++ blockchain.py
@@ -1,4 +1,6 @@
 # Initializing our blockchain list
+MINING_REWARD = 10
+
 genesis_block = {
     'previous_hash': '',
     'index': 0,
@@ -39,6 +41,9 @@ def get_last_blockchain_value():
     return blockchain[-1]
 
 
+# This function accpets two arguments.
+# One required (transition_amount) and one optional (last_transaction)
+# The optional one is optional because it has a default value => [1]
 def add_transaction(recipient, sender=owner, amount=1.0):
     """ Append a new value as well as the last blockchain value to the blockchain.
 
@@ -59,6 +64,12 @@ def add_transaction(recipient, sender=owner, amount=1.0):
 def mine_block():
     last_block = blockchain[-1]
     hashed_block = hash_block(last_block)
+    reward_transaction = {
+        'sender': 'MINING',
+        'recipient': owner,
+        'amount': MINING_REWARD
+    }
+    open_transactions.append(reward_transaction)
     block = {
         'previous_hash': hashed_block,
         'index': len(blockchain),
