3eab742e889d981adbb72ba7f7672f0e3f5029c5
Adel NOUAR <adel.nouar@protonmail.com>
2021-05-25 11:09:10 +0200

    Cleaning code

--- blockchain.py
+++ blockchain.py
@@ -1,11 +1,14 @@
-# Initializing our blockchain list
+import functools
+# The reward we give to miners (for creating a new bloc)
 MINING_REWARD = 10
 
+# Our starting block for the blockchain
 genesis_block = {
     'previous_hash': '',
     'index': 0,
     'transactions': []
 }
+# Initializing our (empty) blockchain list
 blockchain = [genesis_block]
 open_transactions = []
 owner = 'Someone'
@@ -22,17 +25,25 @@ def get_balance(participant):
     open_tx_sender = [tx['amount']
                       for tx in open_transactions if tx['sender'] in participants]
     tx_sender.append(open_tx_sender)
-    amount_sent = 0
-    for tx in tx_sender:
-        if len(tx) > 0:
-            amount_sent += tx[0]
+    amount_sent = functools.reduce(
+        lambda tx_sum, tx_amt: tx_sum +
+        tx_amt[0] if len(tx_amt) > 0 else 0, tx_sender, 0
+    )
+    # amount_sent = 0
+    # for tx in tx_sender:
+    #     if len(tx) > 0:
+    #         amount_sent += tx[0]
 
     tx_recipient = [[tx['amount'] for tx in block['transactions']
                      if tx['recipient'] == participant] for block in blockchain]
-    amount_received = 0
-    for tx in tx_recipient:
-        if len(tx) > 0:
-            amount_received += tx[0]
+    amount_received = functools.reduce(
+        lambda tx_sum, tx_amt: tx_sum +
+        tx_amt[0] if len(tx_amt) > 0 else 0, tx_recipient, 0
+    )
+    # amount_received = 0
+    # for tx in tx_recipient:
+    #     if len(tx) > 0:
+    #         amount_received += tx[0]
 
     return amount_received - amount_sent
 
@@ -179,7 +190,7 @@ while user_choice != 'q':
         print_blockchain_elements()
         print('Invalid blockchain!')
         break
-    print(get_balance(owner))
+    print(f'Balance of {owner}: {get_balance(owner):6.2f}')
 else:
     print('User left!')
 print('Done')
