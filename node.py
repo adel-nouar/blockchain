4630b9513de320e4ed4bd1f016a15c804be3bd67
Adel NOUAR <adel.nouar@protonmail.com>
2021-06-08 08:52:35 +0200

    Adding route with blockchain

--- node.py
+++ node.py
@@ -1,16 +1,28 @@
 from crypt import methods
-from flask import Flask
+from flask import Flask, jsonify
 from wallet import Wallet
 from flask_cors import CORS
+from blockchain import Blockchain
 
 app = Flask(__name__)
 wallet = Wallet()
+blockchain = Blockchain(wallet.public_key)
 CORS(app)
 
 
 @app.route('/', methods=['GET'])
 def get_ui():
     return 'This works!'
+
+
+@app.route('/chain', methods=['GET'])
+def get_chain():
+    chain_snapshot = blockchain.chain
+    dict_chain = [block.__dict__.copy() for block in chain_snapshot]
+    for dict_block in dict_chain:
+        dict_block['transactions'] = [
+            tx.__dict__ for tx in dict_block['transactions']]
+    return jsonify(dict_chain), 200
 
 
 if __name__ == '__main__':
